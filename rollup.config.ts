52cabbf1fbf0a3f09037cde57f398921cc2416de
gniphuo <gniphuo@tencent.com>
2021-07-14 20:05:28 +0800

    build: Update build config

--- rollup.config.ts
+++ rollup.config.ts
@@ -16,7 +16,9 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: [
+    'gifuct-js'
+  ],
   watch: {
     include: 'src/**',
   },

e5cecc089506f85b3d9aa5edb9a9d90b09b6b8d6
gniphuo <gniphuo@tencent.com>
2021-07-15 15:21:38 +0800

    style: Use eslint replace tslint

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json';
+import _ from 'lodash';
 
 const pkg = require('./package.json')
 
@@ -12,7 +12,7 @@ const libraryName = 'gifplay'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.main, name: _.camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -32,9 +32,9 @@ export default {
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+    nodeResolve(),
 
     // Resolve source maps to the original source
-    sourceMaps(),
+    sourcemaps(),
   ],
 }
--- src/gifplay.ts
+++ src/gifplay.ts
@@ -1,173 +1,169 @@
-import { parseGIF, decompressFrames } from 'gifuct-js';
+import { parseGIF, decompressFrames } from 'gifuct-js'
 
 export class GifPlay {
-  playing: boolean = false;
+  playing = false
 
-  paused: boolean = false;
+  paused = false
 
-  loading: boolean = false;
+  loading = false
 
-  gifCanvas: HTMLCanvasElement = document.createElement('canvas');
+  gifCanvas: HTMLCanvasElement = document.createElement('canvas')
 
-  gifCtx: CanvasRenderingContext2D | null = this.gifCanvas.getContext('2d');
+  gifCtx: CanvasRenderingContext2D | null = this.gifCanvas.getContext('2d')
 
-  width: number = 0;
+  width = 0
 
-  height: number = 0;
+  height = 0
 
-  frames: any[] = [];
+  frames: any[] = []
 
-  length: number = 0;
+  length = 0
 
-  frameIndex: number = 0;
+  frameIndex = 0
 
-  playSpeed: number = 1;
+  playSpeed = 1
 
-  imageData: any;
+  imageData: any
 
-  src: string = '';
+  src = ''
 
-  timerId: number = 0;
+  timerId = 0
 
-  rid: number = 0;
+  rid = 0
 
   constructor(url: string, canvas: HTMLCanvasElement) {
-    this.src = url;
-    this.gifCanvas = canvas;
-    this.gifCtx = this.gifCanvas?.getContext('2d');
-    this.load(url);
+    this.src = url
+    this.gifCanvas = canvas
+    this.gifCtx = this.gifCanvas?.getContext('2d')
+    this.load(url)
   }
 
   load(url: string) {
-    this.loading = true;
+    this.loading = true
     fetch(url)
       .then(res => res.arrayBuffer())
       .then(buff => parseGIF(buff))
       .then(gif => {
-        this.frames = decompressFrames(gif, true);
-        this.width = this.frames[0].dims.width;
-        this.height = this.frames[0].dims.height;
-        this.loading = false;
-        this.play();
+        this.frames = decompressFrames(gif, true)
+        this.width = this.frames[0].dims.width
+        this.height = this.frames[0].dims.height
+        this.loading = false
+        this.play()
       })
       .catch(() => {
-        this.loading = false;
-      });
+        this.loading = false
+      })
   }
 
   play() {
     if (this.frames.length < 1) {
-      return;
+      return
     }
     if (!this.playing) {
-      this.paused = false;
-      this.playing = true;
-      this._play();
+      this.paused = false
+      this.playing = true
+      this._play()
     }
   }
 
   pause() {
-    this.paused = true;
-    this.playing = false;
-    clearTimeout(this.timerId);
-    cancelAnimationFrame(this.rid);
+    this.paused = true
+    this.playing = false
+    clearTimeout(this.timerId)
+    cancelAnimationFrame(this.rid)
   }
 
   _play() {
-    let delay;
-    let start = new Date().getTime();
+    let delay
+    const start = new Date().getTime()
     if (this.playSpeed === 0) {
-      return this.pause();
+      return this.pause()
     } else {
-      delay = this.frames[this.frameIndex].delay / this.playSpeed;
+      delay = this.frames[this.frameIndex].delay / this.playSpeed
 
       if (this.frames[this.frameIndex].disposalType === 2) {
-        this.gifCtx?.clearRect(0, 0, this.width, this.height);
+        this.gifCtx?.clearRect(0, 0, this.width, this.height)
       }
 
-      this.imageData = this.frames[this.frameIndex].patch;
+      this.imageData = this.frames[this.frameIndex].patch
       // render canvas
-      this.render();
+      this.render()
       // update frame index
-      this.frameIndex += 1;
+      this.frameIndex += 1
       if (this.frameIndex >= this.frames.length) {
-        this.frameIndex = 0;
+        this.frameIndex = 0
       }
 
-      let end = new Date().getTime();
-      let diff = end - start;
+      const end = new Date().getTime()
+      const diff = end - start
       // timer
       this.timerId = window.setTimeout(() => {
-        this.rid = window.requestAnimationFrame(this._play.bind(this));
-      }, Math.max(0, Math.floor(delay - diff)));
+        this.rid = window.requestAnimationFrame(this._play.bind(this))
+      }, Math.max(0, Math.floor(delay - diff)))
     }
   }
 
   togglePlay() {
     if (this.paused || !this.playing) {
-      this.play();
+      this.play()
     } else {
-      this.pause();
+      this.pause()
     }
   }
 
-  _render() {
-    const dims = this.frames[this.frameIndex].dims;
-    let tempCanvas = document.createElement('canvas');
-    let tempCtx = tempCanvas.getContext('2d');
+  private _render() {
+    const dims = this.frames[this.frameIndex].dims
+    const tempCanvas = document.createElement('canvas')
+    const tempCtx = tempCanvas.getContext('2d')
     if (
       !this.imageData ||
       dims.width != this.imageData.width ||
       dims.height != this.imageData.height
     ) {
-      tempCanvas.width = dims.width;
-      tempCanvas.height = dims.height;
-      this.imageData =
-        tempCtx?.createImageData(dims.width, dims.height) || null;
+      tempCanvas.width = dims.width
+      tempCanvas.height = dims.height
+      this.imageData = tempCtx?.createImageData(dims.width, dims.height) || null
     }
     // set the patch data as an override
-    this.imageData.data.set(this.frames[this.frameIndex].patch);
+    this.imageData.data.set(this.frames[this.frameIndex].patch)
     // draw the patch back over the canvas
-    tempCtx?.putImageData(this.imageData, 0, 0);
+    tempCtx?.putImageData(this.imageData, 0, 0)
 
-    this.gifCtx?.drawImage(tempCanvas, dims.left, dims.top);
+    this.gifCtx?.drawImage(tempCanvas, dims.left, dims.top)
   }
 
   render() {
-    this._render();
-    this._manipulate();
+    this._render()
   }
 
   seek(time: number) {
-    clearTimeout(this.timerId);
-    cancelAnimationFrame(this.rid);
-    if (time < 0) time = 0;
-    time *= 1000; // in ms
-    time %= this.frames[this.frames.length - 1].time;
-    let frame = 0;
+    clearTimeout(this.timerId)
+    cancelAnimationFrame(this.rid)
+    if (time < 0) time = 0
+    time *= 1000 // in ms
+    time %= this.frames[this.frames.length - 1].time
+    let frame = 0
     while (time > this.frames[frame].time && frame < this.frames.length) {
-      frame += 1;
+      frame += 1
     }
-    this.frameIndex = frame;
+    this.frameIndex = frame
     if (this.playing) {
-      this._play();
+      this._play()
     } else {
-      this.imageData = this.frames[this.frameIndex].patch;
-      this.render();
+      this.imageData = this.frames[this.frameIndex].patch
+      this.render()
     }
   }
 
-  seekFrame(frame: number) {
-    clearTimeout(this.timerId);
-    cancelAnimationFrame(this.rid);
-    this.frameIndex = frame % this.frames.length;
+  seekFrame(frame: number): void {
+    clearTimeout(this.timerId)
+    cancelAnimationFrame(this.rid)
+    this.frameIndex = frame % this.frames.length
     if (this.playing) {
-      this._play();
+      this._play()
     } else {
-      this.imageData = this.frames[this.frameIndex].patch;
-      this.render();
+      this.imageData = this.frames[this.frameIndex].patch
+      this.render()
     }
   }
-  // todo
-  _manipulate() {}
 }
